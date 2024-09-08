a32cff43014e86843252879c23e0d77174c52b6b
Anshul Singh <anshulsingh024@yahoo.com>
2024-09-08 18:46:26 +0530

    added CNAME

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className=' bg-black text-white h-20 items-center flex justify-center'>
-      &copy; All rights reserved &nbsp; - &nbsp; sanjanasingh.me (2024 - 25) 
+      &copy; All rights reserved &nbsp; - &nbsp; sanjanasinghwebpage.me (2024 - 25) 
     </div>
   )
 }
